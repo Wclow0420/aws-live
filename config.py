819dcb3b91a6a59a992649f7fa09a32ae7613aa4
Wclow0420 <118270091+Wclow0420@users.noreply.github.com>
2023-12-19 11:44:52 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cgdux6isqeqw.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "low-employee"
+customregion = "us-east-1"
+
